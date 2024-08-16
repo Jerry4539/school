4d9d5ab8535f23340cb90a3f0252d9dfbf5763cc
Jerry <91048755+Jerry4539@users.noreply.github.com>
2024-08-16 09:39:14 +0530

    Add files via upload

---
+++ sections/includes/header.php
@@ -0,0 +1,203 @@
+    <!--==============================
+     Preloader
+  ==============================-->
+    <div class="preloader  ">
+        <button class="vs-btn preloaderCls">Cancel Preloader </button>
+        <div class="preloader-inner">
+            <div class="loader"></div>
+        </div>
+    </div>
+    <!--==============================
+    Mobile Menu
+  ============================== -->
+    <div class="vs-menu-wrapper">
+        <div class="vs-menu-area text-center">
+            <button class="vs-menu-toggle"><i class="fal fa-times"></i></button>
+            <div class="mobile-logo">
+                <a href="index.html"><img src="assets/img/logo.svg" alt="Educino"></a>
+            </div>
+            <div class="vs-mobile-menu">
+                <ul>
+                    <li class="menu-item-has-children">
+                        <a href="index.html">Demos</a>
+                        <ul class="sub-menu">
+                            <li><a href="index.html">Demo Style 1</a></li>
+                            <li><a href="index-2.html">Demo Style 2</a></li>
+                            <li><a href="index-3.html">Demo Style 3</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="about.html">About Us</a>
+                    </li>
+                    <li class="menu-item-has-children">
+                        <a href="course.html">Courses</a>
+                        <ul class="sub-menu">
+                            <li><a href="course.html">Courses 1</a></li>
+                            <li><a href="courses-2.html">Courses 2</a></li>
+                            <li><a href="course-details.html">Courses Details 1</a></li>
+                            <li><a href="course-details-2.html">Courses Details 2</a></li>
+                        </ul>
+                    </li>
+                    <li class="menu-item-has-children">
+                        <a href="blog.html">Blog</a>
+                        <ul class="sub-menu">
+                            <li><a href="blog.html">Blog</a></li>
+                            <li><a href="blog-details.html">Blog Details</a></li>
+                        </ul>
+                    </li>
+                    <li class="menu-item-has-children">
+                        <a href="#">Pages</a>
+                        <ul class="sub-menu">
+                            <li><a href="team.html">Our Tutors</a></li>
+                            <li><a href="team-details.html">Tutor Details</a></li>
+                            <li><a href="become-tutor.html">Become Tutor</a></li>
+                            <li><a href="find-tutor.html">Find Tutor</a></li>
+                            <li><a href="academic.html">Academic</a></li>
+                            <li><a href="academic-program.html">Academic Program</a></li>
+                            <li><a href="program-details.html">Program Details</a></li>
+                            <li><a href="find-program.html">Find Program</a></li>
+                            <li><a href="event-details.html">Event Details</a></li>
+                            <li><a href="login-register.html">Login Register</a></li>
+                            <li><a href="error.html">Error Page</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="contact.html">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    
+    <!--==============================
+    Popup Search Box
+    ============================== -->
+    <div class="popup-search-box d-none d-lg-block  ">
+        <button class="searchClose"><i class="fal fa-times"></i></button>
+        <form action="#">
+            <input type="text" class="border-theme" placeholder="What are you looking for">
+            <button type="submit"><i class="fal fa-search"></i></button>
+        </form>
+    </div>
+    <!--==============================
+    Header Area
+    ==============================-->
+    <header class="vs-header header-layout1">
+        <!-- <div class="header-top">
+            <div class="container">
+                <div class="row justify-content-between align-items-center gx-50">
+                    <div class="col d-none d-xl-block">
+                        <div class="header-links">
+                            <ul>
+                                <li><i class="fas fa-phone-alt"></i>Phone: <a href="+4402076897888">+44 (0) 207 689 7888</a></li>
+                                <li><i class="fas fa-envelope"></i>Email: <a href="mailto:info@company.co.uk">info@company.co.uk</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col col-xl-auto d-none d-md-block">
+                        <a class="user-login" href="login-register.html"><i class="fas fa-user-circle"></i> Login & Register</a>
+                    </div>
+                    <div class="col-md-auto text-center">
+                        <div class="header-social">
+                            <a href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#"><i class="fab fa-twitter"></i></a>
+                            <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> -->
+        <div class="sticky-wrapper">
+            <div class="sticky-active">
+                <div class="container position-relative z-index-common">
+                    <div class="row align-items-center justify-content-between">
+                        <div class="col-auto">
+                            <div class="vs-logo"> <a href="index.html"><img src="assets/img/logo.svg" alt="logo"></a> </div>
+                        </div>
+                        <div class="col text-end text-xl-center">
+                            <nav class="main-menu menu-style1 d-none d-lg-block">
+                                <ul>
+                                    <li class="menu-item-has-children"> <a href="index.html">Demos</a>
+                                        <ul class="sub-menu">
+                                            <li><a href="index.html">Demo Style 1</a></li>
+                                            <li><a href="index-2.html">Demo Style 2</a></li>
+                                            <li><a href="index-3.html">Demo Style 3</a></li>
+                                        </ul>
+                                    </li>
+                                    <li> <a href="about.html">About Us</a> </li>
+                                    <li class="menu-item-has-children">
+                                        <a href="course.html">Courses</a>
+                                        <ul class="sub-menu">
+                                            <li><a href="course.html">Courses 1</a></li>
+                                            <li><a href="courses-2.html">Courses 2</a></li>
+                                            <li><a href="course-details.html">Course Details 1</a></li>
+                                            <li><a href="course-details-2.html">Course Details 2</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="menu-item-has-children">
+                                        <a href="blog.html">Tutor</a>
+                                        <ul class="sub-menu">
+                                            <li><a href="team.html">Our Tutors</a></li>
+                                            <li><a href="team-details.html">Tutor Details</a></li>
+                                            <li><a href="become-tutor.html">Become Tutor</a></li>
+                                            <li><a href="find-tutor.html">Find Tutor</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="menu-item-has-children mega-menu-wrap"> <a href="#">Pages</a>
+                                        <ul class="mega-menu">
+                                            <li><a href="#">Pagelist 1</a>
+                                                <ul>
+                                                    <li><a href="index.html">Demo Style 1</a></li>
+                                                    <li><a href="index-2.html">Demo Style 2</a></li>
+                                                    <li><a href="index-3.html">Demo Style 3</a></li>
+                                                    <li><a href="about.html">About Us</a></li>
+                                                    <li><a href="contact.html">Contact Us</a></li>
+                                                </ul>
+                                            </li>
+                                            <li><a href="#">Pagelist 2</a>
+                                                <ul>
+                                                    <li><a href="course.html">Courses 1</a></li>
+                                                    <li><a href="courses-2.html">Courses 2</a></li>
+                                                    <li><a href="course-details.html">Courses Details 1</a></li>
+                                                    <li><a href="course-details-2.html">Courses Details 2</a></li>
+                                                    <li><a href="event-details.html">Event Details</a></li>
+                                                </ul>
+                                            </li>
+                                            <li><a href="#">Pagelist 3</a>
+                                                <ul>
+                                                    <li><a href="academic.html">Academic</a></li>
+                                                    <li><a href="academic-program.html">Academic Program</a></li>
+                                                    <li><a href="program-details.html">Program Details</a></li>
+                                                    <li><a href="find-program.html">Find Program</a></li>
+                                                    <li><a href="login-register.html">Login Register</a></li>
+                                                    <li><a href="error.html">Error Page</a></li>
+                                                </ul>
+                                            </li>
+                                            <li><a href="#">Pagelist 4</a>
+                                                <ul>
+                                                    <li><a href="team.html">Our Tutors</a></li>
+                                                    <li><a href="team-details.html">Tutor Details</a></li>
+                                                    <li><a href="become-tutor.html">Become Tutor</a></li>
+                                                    <li><a href="find-tutor.html">Find Tutor</a></li>
+                                                    <li><a href="blog.html">Blog</a></li>
+                                                    <li><a href="blog-details.html">Blog Details</a></li>
+                                                </ul>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="contact.html">Contact Us</a></li>
+                                </ul>
+                            </nav>
+                            <button class="vs-menu-toggle d-inline-block d-lg-none"><i class="fal fa-bars"></i></button>
+                        </div>
+                        <div class="col-auto d-none d-xl-block">
+                            <div class="header-btns">
+                                <button type="button" class="searchBoxTggler"><i class="far fa-search"></i></button>
+                                <a href="find-program.html" class="vs-btn style4"><i class="fal fa-graduation-cap"></i> Find Program</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
