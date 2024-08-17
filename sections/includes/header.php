d2be2b5da80735414a2b6ba30f16a2c362272cba
Jerry <91048755+Jerry4539@users.noreply.github.com>
2024-08-17 09:36:21 +0530

    Update header.php

--- sections/includes/header.php
+++ sections/includes/header.php
@@ -1,4 +1,13 @@
-    <!--==============================
+   <style>
+    .menu-item-has-children{
+        color:#000000;
+    }
+    .menu-item-has-children:hover{
+        color:#3747FF;
+    }
+    </style>
+   
+   <!--==============================
      Preloader
   ==============================-->
     <div class="preloader  ">
@@ -14,51 +23,39 @@
         <div class="vs-menu-area text-center">
             <button class="vs-menu-toggle"><i class="fal fa-times"></i></button>
             <div class="mobile-logo">
-                <a href="index.html"><img src="assets/img/logo.svg" alt="Educino"></a>
+                <a href="index.html"><img src="assets/img/logo.svg" alt="Versova Welfare "></a>
             </div>
             <div class="vs-mobile-menu">
                 <ul>
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
                     <li class="menu-item-has-children">
-                        <a href="index.html">Demos</a>
+                        <a>About Us</a>
                         <ul class="sub-menu">
-                            <li><a href="index.html">Demo Style 1</a></li>
-                            <li><a href="index-2.html">Demo Style 2</a></li>
-                            <li><a href="index-3.html">Demo Style 3</a></li>
+                            <li><a href="#">History</a></li>
+                            <li><a href="#">School Song</a></li>
+                            <li><a href="#">Principal Message</a></li>
+                            <li><a href="#">Management Team</a></li>
+                            <li><a href="#">Faculty</a></li>
                         </ul>
                     </li>
                     <li>
-                        <a href="about.html">About Us</a>
+                        <a href="#">Facilities</a>
                     </li>
                     <li class="menu-item-has-children">
-                        <a href="course.html">Courses</a>
+                        <a>Activities</a>
                         <ul class="sub-menu">
-                            <li><a href="course.html">Courses 1</a></li>
-                            <li><a href="courses-2.html">Courses 2</a></li>
-                            <li><a href="course-details.html">Courses Details 1</a></li>
-                            <li><a href="course-details-2.html">Courses Details 2</a></li>
+                            <li><a href="#">Events</a></li>
+                            <li><a href="#">Social Works</a></li>
                         </ul>
                     </li>
                     <li class="menu-item-has-children">
-                        <a href="blog.html">Blog</a>
+                        <a href="#">Achievement</a>
                         <ul class="sub-menu">
-                            <li><a href="blog.html">Blog</a></li>
-                            <li><a href="blog-details.html">Blog Details</a></li>
-                        </ul>
-                    </li>
-                    <li class="menu-item-has-children">
-                        <a href="#">Pages</a>
-                        <ul class="sub-menu">
-                            <li><a href="team.html">Our Tutors</a></li>
-                            <li><a href="team-details.html">Tutor Details</a></li>
-                            <li><a href="become-tutor.html">Become Tutor</a></li>
-                            <li><a href="find-tutor.html">Find Tutor</a></li>
-                            <li><a href="academic.html">Academic</a></li>
-                            <li><a href="academic-program.html">Academic Program</a></li>
-                            <li><a href="program-details.html">Program Details</a></li>
-                            <li><a href="find-program.html">Find Program</a></li>
-                            <li><a href="event-details.html">Event Details</a></li>
-                            <li><a href="login-register.html">Login Register</a></li>
-                            <li><a href="error.html">Error Page</a></li>
+                            <li><a href="#">Sports</a></li>
+                            <li><a href="#">School Awards</a></li>
+                            <li><a href="#">Results</a></li>
                         </ul>
                     </li>
                     <li>
@@ -69,16 +66,7 @@
         </div>
     </div>
     
-    <!--==============================
-    Popup Search Box
-    ============================== -->
-    <div class="popup-search-box d-none d-lg-block  ">
-        <button class="searchClose"><i class="fal fa-times"></i></button>
-        <form action="#">
-            <input type="text" class="border-theme" placeholder="What are you looking for">
-            <button type="submit"><i class="fal fa-search"></i></button>
-        </form>
-    </div>
+    
     <!--==============================
     Header Area
     ==============================-->
@@ -116,84 +104,48 @@
                         </div>
                         <div class="col text-end text-xl-center">
                             <nav class="main-menu menu-style1 d-none d-lg-block">
-                                <ul>
-                                    <li class="menu-item-has-children"> <a href="index.html">Demos</a>
-                                        <ul class="sub-menu">
-                                            <li><a href="index.html">Demo Style 1</a></li>
-                                            <li><a href="index-2.html">Demo Style 2</a></li>
-                                            <li><a href="index-3.html">Demo Style 3</a></li>
-                                        </ul>
-                                    </li>
-                                    <li> <a href="about.html">About Us</a> </li>
-                                    <li class="menu-item-has-children">
-                                        <a href="course.html">Courses</a>
-                                        <ul class="sub-menu">
-                                            <li><a href="course.html">Courses 1</a></li>
-                                            <li><a href="courses-2.html">Courses 2</a></li>
-                                            <li><a href="course-details.html">Course Details 1</a></li>
-                                            <li><a href="course-details-2.html">Course Details 2</a></li>
-                                        </ul>
+                            <ul>
+                                    <li>
+                                        <a href="/">Home</a>
                                     </li>
                                     <li class="menu-item-has-children">
-                                        <a href="blog.html">Tutor</a>
+                                        <a>About Us</a>
                                         <ul class="sub-menu">
-                                            <li><a href="team.html">Our Tutors</a></li>
-                                            <li><a href="team-details.html">Tutor Details</a></li>
-                                            <li><a href="become-tutor.html">Become Tutor</a></li>
-                                            <li><a href="find-tutor.html">Find Tutor</a></li>
+                                            <li><a href="#">History</a></li>
+                                            <li><a href="#">School Song</a></li>
+                                            <li><a href="#">Principal Message</a></li>
+                                            <li><a href="#">Management Team</a></li>
+                                            <li><a href="#">Faculty</a></li>
                                         </ul>
                                     </li>
-                                    <li class="menu-item-has-children mega-menu-wrap"> <a href="#">Pages</a>
-                                        <ul class="mega-menu">
-                                            <li><a href="#">Pagelist 1</a>
-                                                <ul>
-                                                    <li><a href="index.html">Demo Style 1</a></li>
-                                                    <li><a href="index-2.html">Demo Style 2</a></li>
-                                                    <li><a href="index-3.html">Demo Style 3</a></li>
-                                                    <li><a href="about.html">About Us</a></li>
-                                                    <li><a href="contact.html">Contact Us</a></li>
-                                                </ul>
-                                            </li>
-                                            <li><a href="#">Pagelist 2</a>
-                                                <ul>
-                                                    <li><a href="course.html">Courses 1</a></li>
-                                                    <li><a href="courses-2.html">Courses 2</a></li>
-                                                    <li><a href="course-details.html">Courses Details 1</a></li>
-                                                    <li><a href="course-details-2.html">Courses Details 2</a></li>
-                                                    <li><a href="event-details.html">Event Details</a></li>
-                                                </ul>
-                                            </li>
-                                            <li><a href="#">Pagelist 3</a>
-                                                <ul>
-                                                    <li><a href="academic.html">Academic</a></li>
-                                                    <li><a href="academic-program.html">Academic Program</a></li>
-                                                    <li><a href="program-details.html">Program Details</a></li>
-                                                    <li><a href="find-program.html">Find Program</a></li>
-                                                    <li><a href="login-register.html">Login Register</a></li>
-                                                    <li><a href="error.html">Error Page</a></li>
-                                                </ul>
-                                            </li>
-                                            <li><a href="#">Pagelist 4</a>
-                                                <ul>
-                                                    <li><a href="team.html">Our Tutors</a></li>
-                                                    <li><a href="team-details.html">Tutor Details</a></li>
-                                                    <li><a href="become-tutor.html">Become Tutor</a></li>
-                                                    <li><a href="find-tutor.html">Find Tutor</a></li>
-                                                    <li><a href="blog.html">Blog</a></li>
-                                                    <li><a href="blog-details.html">Blog Details</a></li>
-                                                </ul>
-                                            </li>
+                                    <li>
+                                        <a href="#">Facilities</a>
+                                    </li>
+                                    <li class="menu-item-has-children">
+                                        <a>Activities</a>
+                                        <ul class="sub-menu">
+                                            <li><a href="#">Events</a></li>
+                                            <li><a href="#">Social Works</a></li>
                                         </ul>
                                     </li>
-                                    <li><a href="contact.html">Contact Us</a></li>
+                                    <li class="menu-item-has-children">
+                                        <a href="#">Achievement</a>
+                                        <ul class="sub-menu">
+                                            <li><a href="#">Sports</a></li>
+                                            <li><a href="#">School Awards</a></li>
+                                            <li><a href="#">Results</a></li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a href="contact.html">Contact Us</a>
+                                    </li>
                                 </ul>
                             </nav>
                             <button class="vs-menu-toggle d-inline-block d-lg-none"><i class="fal fa-bars"></i></button>
                         </div>
                         <div class="col-auto d-none d-xl-block">
                             <div class="header-btns">
-                                <button type="button" class="searchBoxTggler"><i class="far fa-search"></i></button>
-                                <a href="find-program.html" class="vs-btn style4"><i class="fal fa-graduation-cap"></i> Find Program</a>
+                                <a href="#" class="vs-btn style4"><i class="fal fa-graduation-cap"></i> Admission</a>
                             </div>
                         </div>
                     </div>
